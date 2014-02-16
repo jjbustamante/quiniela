be88b6e414f4c6cb4a360443c0779bf4d12cc8aa
emma <emma@emma-N56VJ.(none)>
2014-02-16 17:41:25 -0430

    Agregando vista de equipos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,17 @@
 class WelcomeController < ApplicationController
   def index
   	@user_session = UserSession.new
+
   	@session = current_user
+
+  	@teamsA = Team.where(group: 'A')
+  	@teamsB = Team.where(group: 'B')
+	@teamsC = Team.where(group: 'C')
+	@teamsD = Team.where(group: 'D')
+	@teamsE = Team.where(group: 'E')
+	@teamsF = Team.where(group: 'F')
+	@teamsG = Team.where(group: 'G')
+	@teamsH = Team.where(group: 'H')
+
   end
 end
