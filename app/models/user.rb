d0f4ae8b0a7087fdc59bee6144b8eee090a3e0f3
emma <emma@emma-N56VJ.(none)>
2014-02-15 20:55:54 -0430

    Agregando AuthLogic para la autenticacion de usuarios, tuve que hacer downgrade de la version de rails a la 3.2.13 para poder usar el AuthLogic

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+
+  acts_as_authentic do |c|
+    c.login_field = 'email'
+
+  end # block optional
+
+  attr_accessible :name, :email, :password, :password_confirmation, :login
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demoapp::Application.config.secret_token = '338a0e9316a63ed5c43dc9550eb5305c3d4497d1f607ab74365dc8752e390fb6853eaeb6f7ec5b46e1606536dfdea54de43e24d6fe99a66397dd379158ff447a'
