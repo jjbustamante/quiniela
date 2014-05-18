91cfb326517726a6cd8c8f016ca08225c4d33388
cheto <cheto@cheto-vm.(none)>
2014-05-17 20:06:44 -0430

    otro commit

--- app/controllers/quinielas_controller.rb
+++ app/controllers/quinielas_controller.rb
@@ -1,5 +1,9 @@
 class QuinielasController < ApplicationController
 	def new
+		@matches = Match.where('').order("id ASC")
+		@session = current_user
+		@active_matches = 'active'
+		
 	end
 
 	def create
