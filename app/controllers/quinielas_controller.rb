961bc608003e798fe890b5ab537923f3f74355e5
emma <emma@emma-N56VJ.(none)>
2014-05-19 22:48:52 -0430

    Agregando validacion de fechas en creacion de quinielas en fase de grupos, se agrego un archivo de propiedades /config/config.file donde debemos agregar las propiedades

--- app/controllers/quinielas_controller.rb
+++ app/controllers/quinielas_controller.rb
@@ -1,16 +1,25 @@
 class QuinielasController < ApplicationController
 	def new
-		@quiniela = Quiniela.new
-		@matches = Match.where('').order("id ASC")
-		@session = current_user
-		@active_matches = 'active'	
-		@bets = []
-		@quiniela.user = current_user
-		@matches.each do |m|
-			bet = @quiniela.bets.build
-			bet.match = m
-			@bets.push bet
+		#deadline = DEADLINE
+		
+		if DateTime.now < APP_CONFIG['deadline_fase_grupos'].to_datetime
+			@quiniela = Quiniela.new
+			@matches = Match.where('').order("id ASC")
+			@session = current_user
+			@active_matches = 'active'	
+			@bets = []
+			@quiniela.user = current_user
+			@matches.each do |m|
+				bet = @quiniela.bets.build
+				bet.match = m
+				@bets.push bet
+			end
+
+		else
+			flash[:success] = "¡La creacion de quinielas ya ha cerrado!"
+			redirect_to '/'
 		end
+		
 	end
 
 	def create
