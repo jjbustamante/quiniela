68a1868d502f00a01b9f33b22b4c6c2fc101da37
Juan Bustamante <bustamantejj@gmail.com>
2014-06-27 22:57:31 -0430

    Ajustes en la puntuacion

--- db/scripts/update_players_score_trigger.sql
+++ db/scripts/update_players_score_trigger.sql
@@ -7,102 +7,65 @@ BEGIN
 		FOR row_data IN SELECT * FROM bets
 		WHERE bets.match_id = new.id LOOP
 			score := 0;
+			-- No es primera ronda
 			IF new.round_id <> 1 THEN
-				IF row_data.team1_id = new.team1_id or row_data.team1_id = new.team2_id THEN
-					score := score + 1;
-				END IF;
-				IF row_data.team2_id = new.team1_id or row_data.team2_id = new.team2_id THEN
-					score := score + 1;
-				END IF;
-				IF (row_data.team1_id = new.team1_id AND row_data.score_t1 = new.score_t1) or (row_data.team1_id = new.team2_id and row_data.score_t1 = new.score_t2) THEN
-					score := score + 1;
-				END IF;
-				IF (row_data.team2_id = new.team1_id AND row_data.score_t2 = new.score_t1) or (row_data.team2_id = new.team2_id and row_data.score_t2 = new.score_t2) THEN
-					score := score + 1;
+				-- Si No son octavos de final y Aciertas el equipo 3 puntos
+				IF new.round_id <> 2 THEN
+					IF row_data.team1_id = new.team1_id or row_data.team1_id = new.team2_id THEN
+						score := score + 3;
+					END IF;
+					IF row_data.team2_id = new.team1_id or row_data.team2_id = new.team2_id THEN
+						score := score + 3;
+					END IF;
 				END IF;
 				
-				IF (row_data.score_t1 > row_data.score_t2) THEN
-					IF (new.score_t1 > new.score_t2) and (row_data.team1_id = new.team1_id) THEN
-						score := score + 2;
-					END IF;
-					IF (new.score_t1 < new.score_t2) and (row_data.team1_id = new.team2_id) THEN
-						score := score + 2;
-					END IF;
-					IF (new.score_t1 = new.score_t2) and (new.winner_id = row_data.team1_id) THEN
-						score := score + 2;
-					END IF;
+				-- Acertar el Score dar 2 puntos
+				IF (row_data.team1_id = new.team1_id AND row_data.score_t1 = new.score_t1) or (row_data.team1_id = new.team2_id and row_data.score_t1 = new.score_t2) THEN
+					score := score + 2;
 				END IF;
-				IF (row_data.score_t1 < row_data.score_t2) THEN
-					IF (new.score_t1 > new.score_t2) and (row_data.team2_id = new.team1_id) THEN
-						score := score + 2;
-					END IF;
-					IF (new.score_t1 < new.score_t2) and (row_data.team2_id = new.team2_id) THEN
-						score := score + 2;
-					END IF;
-					IF (new.score_t1 = new.score_t2) and (new.winner_id = row_data.team2_id) THEN
-						score := score + 2;
-					END IF;
+				IF (row_data.team2_id = new.team1_id AND row_data.score_t2 = new.score_t1) or (row_data.team2_id = new.team2_id and row_data.score_t2 = new.score_t2) THEN
+					score := score + 2;
 				END IF;
-				IF (row_data.score_t1 = row_data.score_t2) THEN
-					IF (new.score_t1 > new.score_t2) and (row_data.winner_id = new.team1_id) THEN
-						score := score + 2;
-					END IF;
-					IF (new.score_t1 < new.score_t2) and (row_data.winner_id = new.team2_id) THEN
-						score := score + 2;
-					END IF;
-					IF (new.score_t1 = new.score_t2) and (new.winner_id = row_data.winner_id) THEN
-						score := score + 2;
-					END IF;
+				
+				IF (new.winner_id = row_data.winner_id) THEN 
+					score := score + 3;
 				END IF;
-
-
+				
+				IF (new.score_t1 = new.score_t2) AND 
+					(row_data.score_t1 = row_data.score_t2) AND
+					((row_data.team1_id = new.team1_id AND row_data.team2_id = new.team2_id ) OR
+					(row_data.team1_id = new.team2_id AND row_data.team2_id = new.team1_id )) THEN
+					score := score + 3;
+				END IF;
+				
 				IF old.score_t1 IS NOT NULL or old.score_t2 IS NOT NULL THEN
-					IF row_data.team1_id = old.team1_id or row_data.team1_id = old.team2_id THEN
-						score := score - 1;
-					END IF;
-					IF row_data.team2_id = old.team1_id or row_data.team2_id = old.team2_id THEN
-						score := score - 1;
-					END IF;
-					IF (row_data.team1_id = old.team1_id AND row_data.score_t1 = old.score_t1) or (row_data.team1_id = old.team2_id and row_data.score_t1 = old.score_t2) THEN
-						score := score - 1;
-					END IF;
-					IF (row_data.team2_id = old.team1_id AND row_data.score_t2 = old.score_t1) or (row_data.team2_id = old.team2_id and row_data.score_t2 = old.score_t2) THEN
-						score := score - 1;
+				
+					IF old.round_id <> 2 THEN
+						IF row_data.team1_id = old.team1_id or row_data.team1_id = old.team2_id THEN
+							score := score - 3;
+						END IF;
+						IF row_data.team2_id = old.team1_id or row_data.team2_id = old.team2_id THEN
+							score := score - 3;
+						END IF;
 					END IF;
 					
-
-					IF (row_data.score_t1 > row_data.score_t2) THEN
-						IF (old.score_t1 > old.score_t2) and (row_data.team1_id = old.team1_id) THEN
-							score := score - 2;
-						END IF;
-						IF (old.score_t1 < old.score_t2) and (row_data.team1_id = old.team2_id) THEN
-							score := score - 2;
-						END IF;
-						IF (old.score_t1 = old.score_t2) and (old.winner_id = row_data.team1_id) THEN
-							score := score - 2;
-						END IF;
+					-- Acertar el Score dar 2 puntos
+					IF (row_data.team1_id = old.team1_id AND row_data.score_t1 = old.score_t1) or (row_data.team1_id = old.team2_id and row_data.score_t1 = old.score_t2) THEN
+						score := score - 2;
 					END IF;
-					IF (row_data.score_t1 < row_data.score_t2) THEN
-						IF (old.score_t1 > old.score_t2) and (row_data.team2_id = old.team1_id) THEN
-							score := score - 2;
-						END IF;
-						IF (old.score_t1 < old.score_t2) and (row_data.team2_id = old.team2_id) THEN
-							score := score - 2;
-						END IF;
-						IF (old.score_t1 = old.score_t2) and (old.winner_id = row_data.team2_id) THEN
-							score := score - 2;
-						END IF;
+					IF (row_data.team2_id = old.team1_id AND row_data.score_t2 = old.score_t1) or (row_data.team2_id = old.team2_id and row_data.score_t2 = old.score_t2) THEN
+						score := score - 2;
 					END IF;
-					IF (row_data.score_t1 = row_data.score_t2) THEN
-						IF (old.score_t1 > old.score_t2) and (row_data.winner_id = old.team1_id) THEN
-							score := score - 2;
-						END IF;
-						IF (old.score_t1 < old.score_t2) and (row_data.winner_id = old.team2_id) THEN
-							score := score - 2;
-						END IF;
-						IF (old.score_t1 = old.score_t2) and (old.winner_id = row_data.winner_id) THEN
-							score := score - 2;
-						END IF;
+					
+					IF (old.winner_id = row_data.winner_id) THEN 
+						score := score - 3;
+					END IF;
+					
+					IF (old.score_t1 = old.score_t2) AND 
+						(row_data.score_t1 = row_data.score_t2) AND
+						((row_data.team1_id = old.team1_id AND row_data.team2_id = old.team2_id ) OR
+						(row_data.team1_id = old.team2_id AND row_data.team2_id = old.team1_id )) THEN
+							score := score - 3;
 					END IF;
 
 				END IF;
