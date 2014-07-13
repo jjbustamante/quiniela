04babeda58660f11eaaa7f031b993ff636613c67
emma <emma@emma-N56VJ.(none)>
2014-07-13 17:50:36 -0430

    ranking en pagina principal

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,12 @@
 class WelcomeController < ApplicationController
   def index
-  	@session = current_user
-
+  	@quinielas = Quiniela.where('').order("points DESC").limit(3)
+	@active_ranking = 'active'
+	@session = current_user
+	@no_top = true
+	if !@session
+		redirect_to "/"
+	end
   end
 
   before_filter :authenticate_user!
